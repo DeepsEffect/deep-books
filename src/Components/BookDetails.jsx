29f467529a72ade9bea608beb9dd14ed738b759f
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-03-26 18:08:40 +0600

    book details section added

--- src/Components/BookDetails.jsx
+++ src/Components/BookDetails.jsx
@@ -1,13 +1,70 @@
-import { useLoaderData, useParams,  } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 
 const BookDetails = () => {
   const bookData = useLoaderData();
-  const {bookId} = useParams();
-  const book = bookData.find(book => book.bookId == bookId);
-//   console.log(book);
+  const { bookId } = useParams();
+  const book = bookData.find((book) => book.bookId == bookId);
+  const {
+    bookName,
+    author,
+    category,
+    review,
+    tags,
+    totalPages,
+    publisher,
+    yearOfPublishing,
+    rating,
+    image,
+  } = book;
+
   return (
-    <div>
-      <h2>bookName: {book.bookName}</h2>
+    <div className="hero bg-base-200 rounded-2xl mt-4">
+      <div className="hero-content flex-col lg:flex-row">
+        <img src={image} className="w-full rounded-lg shadow-2xl" />
+        <div className="ml-12">
+          <h1 className="text-5xl font-bold">{bookName}</h1>
+          <p>by: {author}</p>
+          <p>{category}</p>
+          <div className="divider"></div>
+          <p className="py-6">
+            <span className="font-bold">Review: </span>
+            {review}
+          </p>
+          <div className="card-actions flex flex-col">
+            <div className="flex gap-4 items-center">
+              <span className="font-bold">Tags: </span>
+              {tags.map((tag, idx) => (
+                <div className="badge badge-outline badge-success" key={idx}>
+                  {tag}
+                </div>
+              ))}
+            </div>
+            <div className="divider"></div>
+            <div>
+              <p>
+                <span>number of pages: </span>
+                {totalPages}
+              </p>
+              <p>
+                <span>Publisher: </span>
+                {publisher}
+              </p>
+              <p>
+                <span>Year Of Publishing: </span>
+                {yearOfPublishing}
+              </p>
+              <p>
+                <span>Rating: </span>
+                {rating}
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-4 mt-4">
+            <button className="btn btn-outline">Read</button>
+            <button className="btn btn-accent">Wishlist</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
