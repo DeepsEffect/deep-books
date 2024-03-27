0f4a503a27930f74df8ff8efe8d9b70d95e43fae
DeepsEffect <jalal.ahmed.dev@gmail.com>
2024-03-28 00:39:23 +0600

    header for the listed books and dropdown for sorting

--- src/Pages/ListedBooks.jsx
+++ src/Pages/ListedBooks.jsx
@@ -5,11 +5,36 @@ const ListedBooks = () => {
   const [tabIndex, setTabIndex] = useState(0);
   return (
     <div>
+      <div className="font-bold text-2xl text-center bg-gray-200 py-4 rounded-xl mb-6">
+        <h2>Books</h2>
+      </div>
+      {/* sort by menu */}
+      <div className="dropdown flex justify-center">
+        <div tabIndex={0} role="button" className="btn btn-success text-white m-1">
+          Click
+        </div>
+        <ul
+          tabIndex={0}
+          className="dropdown-content z-[1] menu p-2 mt-16 shadow bg-base-100 rounded-box w-52"
+        >
+          <li>
+            <a>Ratings</a>
+          </li>
+          <li>
+            <a>Number Of Pages</a>
+          </li>
+          <li>
+            <a>Published Year</a>
+          </li>
+        </ul>
+      </div>
       <div className="flex items-start -mx-4 overflow-x-auto overflow-y-hidden sm:justify-start flex-nowrap  dark:text-gray-800">
         <Link
-          to={''}
-          onClick={()=> setTabIndex(0)}
-          className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 0 ? 'border border-b-0' : 'border-b'} rounded-t-lg dark:border-gray-600 dark:text-gray-900`}
+          to={""}
+          onClick={() => setTabIndex(0)}
+          className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${
+            tabIndex === 0 ? "border border-b-0" : "border-b"
+          } rounded-t-lg dark:border-gray-600 dark:text-gray-900`}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -26,9 +51,11 @@ const ListedBooks = () => {
           <span>Read Books</span>
         </Link>
         <Link
-          to={'wishlist'}
-          onClick={()=> setTabIndex(1)}
-          className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 1 ? 'border border-b-0' : 'border-b'} rounded-t-lg dark:border-gray-600 dark:text-gray-900`}
+          to={"wishlist"}
+          onClick={() => setTabIndex(1)}
+          className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${
+            tabIndex === 1 ? "border border-b-0" : "border-b"
+          } rounded-t-lg dark:border-gray-600 dark:text-gray-900`}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
